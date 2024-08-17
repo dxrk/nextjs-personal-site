54575ce4da9bb7b0f745c261a144543feed0e27d
ben <bentalesnik@gmail.com>
2024-08-17 17:23:03 -0400

    chore: Update private key in gs-action.ts for better security

--- app/bbyo-utils/movement-launch/gs-action.ts
+++ app/bbyo-utils/movement-launch/gs-action.ts
@@ -8,7 +8,7 @@ export async function getSheetData() {
       type: "service_account",
       project_id: process.env.PROJECT_ID,
       private_key_id: process.env.PRIVATE_KEY_ID,
-      private_key: process.env.PRIVATE_KEY,
+      private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n") ?? "",
       client_email: process.env.CLIENT_EMAIL,
       universe_domain: "googleapis.com",
     },
