14c1faa6da85738df7c636b43f4cfcac106defd0
ben <bentalesnik@gmail.com>
2024-08-27 12:11:04 -0400

    Created a home view for the execs tools

--- app/bbyo-utils/execs/room-checks/layout.tsx
+++ app/bbyo-utils/execs/room-checks/layout.tsx
@@ -7,11 +7,11 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BBYO Utilities",
-  description: "A collection of utilities for BBYO.",
+  title: "BBYO August Execs 2024",
+  description: "Room Checks",
 };
 
-export default function ExecsLayout({
+export default function CheckInLayout({
   children,
 }: {
   children: React.ReactNode;
