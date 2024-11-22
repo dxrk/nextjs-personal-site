9706a96283b3423413e29e07cf7f4186bd4b6fd5
ben <bentalesnik@gmail.com>
2024-11-22 13:15:17 -0500

    Refactor Strava API integration to remove unused imports and commented code Refactor Strava API integration to include NextApiRequest and NextApiResponse types Refactor Strava API integration to improve performance and error handling Refactor room checks layout to update title Refactor CheckInScreen component to fix styling issue Delete unused route file for getting Strava stats Delete unused route file for refreshing Strava token

--- app/api/strava/refreshToken/route.ts
+++
@@ -1,8 +0,0 @@
-// import { NextResponse } from "next/server";
-// import { NextApiRequest, NextApiResponse } from "next/types";
-
-// export async function GET(req: NextApiRequest, res: NextApiResponse) {
-//   const response = await fetch("https://www.strava.com/oauth/token", {
-//     method: "POST",
-//   });
-// }
