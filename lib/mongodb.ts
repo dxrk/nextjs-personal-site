82f8cccf2771baa899e3753ba2cc9bd1bad4f13f
ben <bentalesnik@gmail.com>
2025-01-17 15:34:36 -0500

    Refactor mongodb.ts to simplify connection logic

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -10,20 +10,20 @@ const uri = process.env.MONGODB_URI;
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === "development") {
-  let globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise?: Promise<MongoClient>;
-  };
+// if (process.env.NODE_ENV === "development") {
+//   let globalWithMongo = global as typeof globalThis & {
+//     _mongoClientPromise?: Promise<MongoClient>;
+//   };
 
-  if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri);
-    globalWithMongo._mongoClientPromise = client.connect();
-  }
-  clientPromise = globalWithMongo._mongoClientPromise;
-} else {
-  client = new MongoClient(uri);
-  clientPromise = client.connect();
-}
+//   if (!globalWithMongo._mongoClientPromise) {
+//     client = new MongoClient(uri);
+//     globalWithMongo._mongoClientPromise = client.connect();
+//   }
+//   clientPromise = globalWithMongo._mongoClientPromise;
+// } else {
+client = new MongoClient(uri);
+clientPromise = client.connect();
+// }
 
 export async function connectToMongoDB() {
   const client = await clientPromise;
