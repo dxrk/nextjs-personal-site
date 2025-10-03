9f4cd5922dfbfd09a2704055bca00ca32d89035c
Benjamin Spencer Talesnik <bentalesnik@gmail.com>
2025-10-03 12:20:37 -0400

    Remove BBYO Utilities components and assets
    - Deleted layout, page, and data files for BBYO Utilities, including assignments, awards, charters, and execs.
    - Removed associated assets such as images and CSV templates.
    - Cleaned up the project structure by eliminating unused components.

--- app/bbyo-utils/ic-launch/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "IC Launch",
-};
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
